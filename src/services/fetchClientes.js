7995849082721ad4f8145f589b798cbd9bdbd724
Jua Jose Taboada <juanjo.fra@gmail.com>
2021-03-14 18:10:33 -0300

    Fetch url produccion

--- src/services/fetchClientes.js
+++ src/services/fetchClientes.js
@@ -3,13 +3,13 @@ const URLdev = 'http://localhost:3300'
 
 export const getClientes = (page = 0) => {
   console.log(page, 'getClientes');
-  // return fetch(`${URL}/api/clientes`).then(res => res.json());
-  return page === 0 ? fetch(`${URLdev}/api/clientes`).then(res => res.json()) : fetch(`${URLdev}/api/clientes?page=${page}`).then(res => res.json());
+  return fetch(`${URL}/api/clientes`).then(res => res.json());
+  // return page === 0 ? fetch(`${URLdev}/api/clientes`).then(res => res.json()) : fetch(`${URLdev}/api/clientes?page=${page}`).then(res => res.json());
 }
 
 export const setCliente = (values) => {
-  // const url = `${URL}/api/clientes/register`;
-  const url = `${URLdev}/api/clientes/register`;
+  const url = `${URL}/api/clientes/register`;
+  // const url = `${URLdev}/api/clientes/register`;
 
      return fetch(url, {
         method: 'POST', 
@@ -22,6 +22,6 @@ export const setCliente = (values) => {
 }
 
 export const searchCliente = (name='', ci='', operator='') => {
-  // return fetch(`${URL}/api/clientes/buscador?name=${name}&ci=${ci}&operator=${operator}`).then(res => res.json());
-  return fetch(`${URLdev}/api/clientes/buscador?name=${name}&ci=${ci}&operator=${operator}`).then(res => res.json());
+  return fetch(`${URL}/api/clientes/buscador?name=${name}&ci=${ci}&operator=${operator}`).then(res => res.json());
+  // return fetch(`${URLdev}/api/clientes/buscador?name=${name}&ci=${ci}&operator=${operator}`).then(res => res.json());
 }
